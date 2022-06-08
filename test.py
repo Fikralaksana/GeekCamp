3dd005c9a28dade0d55cbbb4ad796f0951810b8a
ReddyAdhi <reddyadhi94@gmail.com>
2022-06-08 14:55:33 +0700

    Test Commit

--- test.py
+++ test.py
@@ -123,16 +123,16 @@ else:
 
 
   
-print(x)
-"""ssssssssssssssssssssssssss"""
-<<<<<<< HEAD
-2
+# print(x)
+# """ssssssssssssssssssssssssss"""
+# <<<<<<< HEAD
+# 2
 
-=======
-1
->>>>>>> 85351c9b3bb87dcbeb2fe14a732ab10738532989
+# =======
+# 1
+# >>>>>>> 85351c9b3bb87dcbeb2fe14a732ab10738532989
 
-
+print("Hello World")
 
 
 
